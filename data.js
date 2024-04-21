ac787449b5e651487f0cbc4a1d86d04addf84800
Divyanshvj <divyanshvijay24@gmail.com>
2024-04-22 01:12:07 +0530

    commit for Sql and CURL commands

--- data.js
+++ data.js
@@ -10,13 +10,24 @@
 // insert into info values(2, 8 , 92 );
 // select * from info;
 
-// CURL commands
+// CURL commands for user 
 
 // get 
 // curl -v http://localhost:4500/user/all
 // post
 // curl -d "{\"name\":\"riya\", \"dob\":\"2023-01-01\", \"email\":\"riya@gmail\", \"phone\":9829014299, \"address\":\"kota\"}" -H "Content-Type: application/json" http://localhost:4500/user/insert
 // Put
-
+// curl -X PUT   -d "{\"name\":\"Priya\", \"dob\":\"2023-01-01\", \"email\":\"riya@gmail\"}"  -H 'Content-Type:application/json' http://localhost:4500/user/update/9829014299
 // delete
 // curl -X DELETE http://localhost:4500/user/delete/98845432 
+
+// CURL commands for info
+
+// get 
+// curl -v http://localhost:4500/info/all
+// post
+// curl -d "{\"rollno\":2, \"semester\": 3, \"marks\":85}" -H "Content-Type: application/json" http://localhost:4500/info/insert
+// Put
+// curl -X PUT   -d "{\"semester\": 4, \"marks\":87}" -H 'Content-Type:application/json' http://localhost:4500/info/update/2
+// delete
+// curl -X DELETE http://localhost:4500/info/delete/2 
